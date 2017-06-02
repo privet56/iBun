15a7105678b9041aa6c1e2c56c0d58ebeb7f74d9
h <h@hs-Mac.local>
2017-06-02 15:01:39 +0200

    Initial Commit

---
+++ iBun/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  iBun
+//
+//  Created by h on 02.06.17.
+//  Copyright © 2017 h. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
