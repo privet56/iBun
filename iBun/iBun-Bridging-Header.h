abff36fde76cdf94c4a57ad7232ec1660fb9dbaf
h <h@hs-Mac.local>
2017-06-06 13:38:44 +0200

    page1

--- iBun/iBun-Bridging-Header.h
+++ iBun/iBun-Bridging-Header.h
@@ -2,5 +2,6 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+//source: https://github.com/mayoff/uiimage-from-animated-gif
 #import "UIImage+animatedGIF.h"
 
