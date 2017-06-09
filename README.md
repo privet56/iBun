e54460d332f5aa4a48a4b4a8546e0ba4c09c8687
privet <h@hs-Mac.local>
2017-06-09 12:40:07 +0200

    page with carousel

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ iBun - an iOS App with Swift
 
 
 ![iBun](https://raw.githubusercontent.com/privet56/iBun/master/ibun.gif)
+
+TODO:
+- carousel back button
+- carousel background dynamic/transparent
+- carousel onSelect maximize pic
+- arrow animation: 3DConcat translate & scale
+- SpriteKit - 2D game
+- SceneKit - 3D game
+- windows build with VS
--- iBun/CarouselViewController.swift
+++ iBun/CarouselViewController.swift
@@ -12,7 +12,13 @@ import Foundation
 
 class CarouselViewController: UIViewController, iCarouselDataSource, iCarouselDelegate
 {
+    let LINEARIZEIMGPATHS = true;
+    
     var topics:[String:Int] = ["autumn":3, "home":12, "spring":4,"summer":4,"winter":7];
+    var imgpaths:[String] = [];
+    var bkgImage:UIImage? = nil
+    var bkgImageView:UIImageView? = nil
+    
     @IBOutlet var carousel: iCarousel!
     
     override func awakeFromNib()
@@ -23,9 +29,13 @@ class CarouselViewController: UIViewController, iCarouselDataSource, iCarouselDe
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        carousel.type = .coverFlow
-        carousel.dataSource = self
-        carousel.delegate = self
+        
+        for topic in self.topics
+        {
+            let indexWithLeadingZero = topic.value > 9 ? String(topic.value) : String(format: "%02d", topic.value)
+            let path = "buns/"+topic.key+indexWithLeadingZero+".png";
+            imgpaths.append(path)
+        }
         
         do
         {
@@ -40,32 +50,72 @@ class CarouselViewController: UIViewController, iCarouselDataSource, iCarouselDe
         {
             if let path = Bundle.main.path(forResource:"water", ofType:"gif")
             {
-                var image:UIImage? = nil
-                var stretch:Bool = true
-                if(!stretch)
-                {
-                    let url : URL = URL.init(fileURLWithPath: path)
-                    image = UIImage.animatedImage(withAnimatedGIFURL:url)
-                }
-                else
-                {
-                    UIGraphicsBeginImageContext(self.view.frame.size)
-                    image = UIImage(named:path)
-                    image!.draw(in:self.view.bounds)
-                    image = UIGraphicsGetImageFromCurrentImageContext()
-                    UIGraphicsEndImageContext()
-                }
+                let url : URL = URL.init(fileURLWithPath: path)
+                self.bkgImage = UIImage.animatedImage(withAnimatedGIFURL:url)
+                UIGraphicsBeginImageContext(self.view.frame.size)
+                self.bkgImage!.draw(in:self.view.bounds)
+                let image:UIImage? = UIGraphicsGetImageFromCurrentImageContext()
+                UIGraphicsEndImageContext()
                 self.view.backgroundColor = UIColor(patternImage:image!)
+                
+                //self.view.backgroundColor = UIColor.clear //result is black
+                //aniBackground(gifimage:image!)
+                
+                self.bkgImageView = UIImageView(image: self.bkgImage!)
+                self.bkgImageView!.frame.size = self.view.frame.size
+                //self.view.addSubview(self.bkgImageView!)
+                //self.bkgImageView!.isHidden = true
+
+                /*var timer = Timer.scheduledTimer(timeInterval:0.4, target: self,
+                                                     selector: #selector(CarouselViewController.update), userInfo: nil, repeats: true)*/
+                
             }
             else
             {
                 print("ERR: no water");
             }
         }
+        
+        carousel.type = .coverFlow
+        carousel.dataSource = self
+        carousel.delegate = self
+    }
+    func update()
+    {
+        UIGraphicsBeginImageContext(self.bkgImageView!.frame.size)
+        self.bkgImageView!.layer.render(in: UIGraphicsGetCurrentContext()!)
+        let image:UIImage? = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        if(image != nil)
+        {
+            self.view.backgroundColor = UIColor(patternImage:image!)
+        }
+    }
+    func aniBackground(gifimage:UIImage) -> Void
+    {
+        //self.view.backgroundColor = UIColor(patternImage:gifimage)
+        
+        let anAnimation:CABasicAnimation = CABasicAnimation(keyPath:"backgroundColor");
+        anAnimation.duration = 1.00;
+        anAnimation.repeatCount = 999;
+        anAnimation.autoreverses = false;
+        anAnimation.duration = 3.3
+        anAnimation.repeatDuration = 1.3
+        anAnimation.beginTime = CACurrentMediaTime()
+        anAnimation.fillMode = kCAFillModeBackwards
+        anAnimation.timingFunction = CAMediaTimingFunction(name:kCAMediaTimingFunctionEaseInEaseOut)
+
+        anAnimation.fromValue = UIColor.red.cgColor//UIColor(patternImage:gifimage).cgColor
+        anAnimation.toValue = UIColor.green.cgColor//UIColor(patternImage:gifimage)
+        self.view.layer.add(anAnimation, forKey: nil)
     }
     
     func numberOfItems(in carousel: iCarousel) -> Int
     {
+        #if LINEARIZEIMGPATHS
+            return self.imgpaths.count
+        #endif
+        
         var len:Int = 0
         for topic in self.topics
         {
@@ -91,9 +141,13 @@ class CarouselViewController: UIViewController, iCarouselDataSource, iCarouselDe
     
     func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView
     {
-        let topic:(key: String, val: Int) = getTopicAndIndex(index:index)
-        let indexWithLeadingZero = topic.val > 9 ? String(topic.val) : String(format: "%02d", topic.val)
-        let path = "buns/"+topic.key+indexWithLeadingZero+".png";
+        #if LINEARIZEIMGPATHS
+            let path = self.imgpaths[index]
+        #else
+            let topic:(key: String, val: Int) = getTopicAndIndex(index:index)
+            let indexWithLeadingZero = topic.val > 9 ? String(topic.val) : String(format: "%02d", topic.val)
+            let path = "buns/"+topic.key+indexWithLeadingZero+".png";
+        #endif
         
         let itemView = UIImageView(frame: CGRect(x: 0, y: 0, width: (self.view.bounds.width / 3) * 2, height: (self.view.bounds.height / 3) * 2))
         itemView.image = UIImage(named: path)
